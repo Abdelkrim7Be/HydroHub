7f6dda07fc6c55866515b89a19c84f5ecd60db02
Abdelkrim7Be <abdelkrimbellagnech99@gmail.com>
2024-07-22 02:52:05 +0100

    Fixing CORS Policy Bug & Body & Cookie Parser Installed

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+
+// to prevent that the req.body being undefined
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.use("/api", require("./routes/authRoutes"));
 
