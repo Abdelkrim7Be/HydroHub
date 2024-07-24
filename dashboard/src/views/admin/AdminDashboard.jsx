8a9d9569be81ddfdd137fcaf0fd658a74d9e213a
Abdelkrim7Be <abdelkrimbellagnech99@gmail.com>
2024-07-24 13:19:19 +0100

    Admin Dashboard Design #1 : Cards Grid Implemented

--- dashboard/src/views/admin/AdminDashboard.jsx
+++ dashboard/src/views/admin/AdminDashboard.jsx
@@ -1,9 +1,51 @@
 import React from "react";
+import { FaUsers } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
+import { MdCurrencyExchange, MdProductionQuantityLimits } from "react-icons/md";
 
 const AdminDashboard = () => {
   return (
     <div>
-      <h1>Admin Dashboard</h1>
+      <div className="px-2 md:px-7 py-5">
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-7">
+          <div className="flex justify-between items-center p-5 bg-[#e2e2e2] rounded-md gap-3">
+            <div className="flex flex-col justify-start items-start text-[#1e1e2c]">
+              <h2 className="text-3xl font-semibold">$7548</h2>
+              <span className="text-md font-light">Total Sales</span>
+            </div>
+            <div className="w-[40px] h-[40px] rounded-full bg-[#f29f6782] flex justify-center items-center text-xl">
+              <MdCurrencyExchange className="shadow-lg" />
+            </div>
+          </div>
+          <div className="flex justify-between items-center p-5 bg-[#e2e2e2] rounded-md gap-3">
+            <div className="flex flex-col justify-start items-start text-[#1e1e2c]">
+              <h2 className="text-3xl font-semibold">50</h2>
+              <span className="text-md font-light">Products</span>
+            </div>
+            <div className="w-[40px] h-[40px] rounded-full bg-[#f29f6782] flex justify-center items-center text-xl">
+              <MdProductionQuantityLimits className="shadow-lg" />
+            </div>
+          </div>
+          <div className="flex justify-between items-center p-5 bg-[#e2e2e2] rounded-md gap-3">
+            <div className="flex flex-col justify-start items-start text-[#1e1e2c]">
+              <h2 className="text-3xl font-semibold">10</h2>
+              <span className="text-md font-light">Sellers</span>
+            </div>
+            <div className="w-[40px] h-[40px] rounded-full bg-[#f29f6782] flex justify-center items-center text-xl">
+              <FaUsers className="shadow-lg" />
+            </div>
+          </div>
+          <div className="flex justify-between items-center p-5 bg-[#e2e2e2] rounded-md gap-3">
+            <div className="flex flex-col justify-start items-start text-[#1e1e2c]">
+              <h2 className="text-3xl font-semibold">82</h2>
+              <span className="text-md font-light">Orders</span>
+            </div>
+            <div className="w-[40px] h-[40px] rounded-full bg-[#f29f6782] flex justify-center items-center text-xl">
+              <FaCartShopping className="shadow-lg" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
