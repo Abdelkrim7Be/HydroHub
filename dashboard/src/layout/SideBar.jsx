d49c10d68340c8216e871171d979d340b3e87674
Abdelkrim7Be <abdelkrimbellagnech99@gmail.com>
2024-07-24 01:03:40 +0100

    Dynamic Shared-Across-Dashboards Sidebar Implemented : Design & Routing Added

--- dashboard/src/layout/SideBar.jsx
+++ dashboard/src/layout/SideBar.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { getNav } from "../navigation/index";
+import { BiLogOutCircle } from "react-icons/bi";
 
 const SideBar = () => {
+  const { pathname } = useLocation();
+  const [allNav, setAllNav] = useState([]);
+
+  useEffect(() => {
+    const navs = getNav("admin");
+    setAllNav(navs);
+  }, []);
+
   return (
-    <div>
-      <h1>Side Bar</h1>
+    <div className="fixed top-0 left-0 w-[260px] h-screen bg-[#e2e2e2] shadow-md z-50 transition-all flex flex-col justify-between">
+      <div>
+        <div className="h-[100px] flex justify-center items-center">
+          <Link to="/" className="w-[180px] h-[50px]">
+            <img src="http://localhost:3000/images/fullLogo.png" alt="logo" />
+          </Link>
+        </div>
+        <div className="mt-4 px-[16px] pl-0">
+          <ul>
+            {allNav.map((n, i) => (
+              <li key={i} className="mb-2">
+                <Link
+                  to={n.path}
+                  className={`flex items-center gap-[12px] px-[12px] py-[9px] rounded-r-full transition-all duration-200 w-full ${
+                    pathname === n.path
+                      ? "bg-[#f29f6782] text-white shadow-lg"
+                      : "text-[#1e1e2c] font-medium text-sm hover:bg-[#f29f6747] hover:text-white"
+                  }`}
+                >
+                  <span>{n.icon}</span>
+                  <span className="text-sm">{n.title}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="mb-4 px-[16px] pl-0">
+        <button className="flex items-center gap-[12px] px-[12px] py-[9px] rounded-r-full w-full text-[#1e1e2c] font-medium text-sm hover:bg-[#f29f6747] hover:text-white transition-all duration-200">
+          <BiLogOutCircle />
+          <span>Sign out</span>
+        </button>
+      </div>
     </div>
   );
 };
---
+++ dashboard/src/navigation/allNav.js
@@ -0,0 +1,71 @@
+import { MdDashboard, MdPayment } from "react-icons/md";
+import { BiCategory } from "react-icons/bi";
+import { FaUser, FaUsers, FaUserTimes } from "react-icons/fa";
+import { FaCodePullRequest } from "react-icons/fa6";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { IoIosChatbubbles } from "react-icons/io";
+export const allNav = [
+  {
+    id: 1,
+    title: "Dashboard",
+    icon: <MdDashboard />,
+    role: "admin",
+    path: "/admin/dashboard",
+  },
+  {
+    id: 2,
+    title: "Orders",
+    icon: <AiOutlineShoppingCart />,
+    role: "admin",
+    path: "/admin/dashboard/orders",
+  },
+  {
+    id: 3,
+    title: "Category",
+    icon: <BiCategory />,
+    role: "admin",
+    path: "/admin/dashboard/orders",
+  },
+  {
+    id: 4,
+    title: "Sellers",
+    icon: <FaUsers />,
+    role: "admin",
+    path: "/admin/dashboard/sellers",
+  },
+  {
+    id: 5,
+    title: "Payment Requests",
+    icon: <MdPayment />,
+    role: "admin",
+    path: "/admin/dashboard/payment-requests",
+  },
+  {
+    id: 6,
+    title: "Deactivate Seller",
+    icon: <FaUserTimes />,
+    role: "admin",
+    path: "/admin/dashboard/deactivate-seller",
+  },
+  {
+    id: 7,
+    title: "Seller Requests",
+    icon: <FaCodePullRequest />,
+    role: "admin",
+    path: "/admin/dashboard/deactive-sellers",
+  },
+  {
+    id: 8,
+    title: "Profil",
+    icon: <FaUser />,
+    role: "admin",
+    path: "/admin/dashboard/profil",
+  },
+  {
+    id: 9,
+    title: "Live Chat",
+    icon: <IoIosChatbubbles />,
+    role: "admin",
+    path: "/admin/dashboard/chat-seller",
+  },
+];
