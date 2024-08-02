41a81bfa981da7efab4d708525eb3d7c731783f6
Abdelkrim7Be <abdelkrimbellagnech99@gmail.com>
2024-08-02 02:00:31 +0100

    Register & Login Pages : Styling Enhancements Added

--- dashboard/src/views/auth/Login.jsx
+++ dashboard/src/views/auth/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaEnvelope, FaLock } from "react-icons/fa";
 
 const Login = () => {
   const [state, setState] = useState({
@@ -10,7 +11,7 @@ const Login = () => {
   const inputHandler = (e) => {
     setState({
       ...state,
-      [e.target.name]: [e.target.value],
+      [e.target.name]: e.target.value,
     });
   };
 
@@ -18,21 +19,28 @@ const Login = () => {
     e.preventDefault();
     console.log(state);
   };
+
   return (
     <div className="font-[sans-serif] bg-gray-50 flex items-center md:h-screen p-4 flex-col">
       <div>
-        <img src="/HMM.png" className="w-32 mx-auto" alt="Logo" />
+        <img
+          src="http://localhost:3000/images/logo.png"
+          className="w-32 mx-auto"
+          alt="Logo"
+        />
       </div>
       <div className="w-full max-w-4xl max-md:max-w-xl mx-auto">
-        <div className="bg-white grid md:grid-cols-2 gap-16 w-full sm:p-8 p-6 shadow-md rounded-md overflow-hidden">
+        <div className="bg-[#e2e2e2] grid md:grid-cols-2 gap-16 w-full sm:p-8 p-6 shadow-md rounded-md overflow-hidden">
           <div className="max-md:order-1 space-y-6">
             <div className="md:mb-16 mb-8">
-              <h3 className="text-gray-800 text-2xl font-bold">Welcome Back</h3>
+              <h3 className="text-[#1e1e2c] text-2xl font-bold">
+                Welcome Back
+              </h3>
             </div>
             <div className="space-y-6">
               <button
                 type="button"
-                className="w-full px-5 py-2.5 flex items-center justify-center rounded-md text-white text-base tracking-wider font-semibold border-none outline-none bg-blue-600 hover:bg-blue-700"
+                className="w-full px-5 py-2.5 flex items-center justify-center rounded-md text-white text-base tracking-wider font-semibold border-none outline-none bg-[#c28f6d] hover:shadow-slate-300 hover:shadow-md"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -50,7 +58,7 @@ const Login = () => {
               </button>
               <button
                 type="button"
-                className="w-full px-5 py-2.5 flex items-center justify-center rounded-md text-gray-800 text-base tracking-wider font-semibold border-none outline-none bg-gray-100 hover:bg-gray-200"
+                className="w-full px-5 py-2.5 flex items-center justify-center rounded-md text-[#1e1e2c] text-base tracking-wider font-semibold border-none outline-none bg-gray-100 hover:shadow-slate-100/90"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -97,81 +105,57 @@ const Login = () => {
 
           <form className="w-full" onSubmit={submit}>
             <div className="mb-8">
-              <h3 className="text-gray-800 text-2xl font-bold">Login</h3>
+              <h3 className="text-[#1e1e2c] text-2xl font-bold">Login</h3>
             </div>
             <div className="space-y-6">
               <div>
-                <label className="text-gray-800 text-sm mb-2 block">
+                <label className="text-[#1e1e2c] text-sm mb-2 block">
                   Email
                 </label>
-                <div className="relative flex items-center">
+                <div className="bg-white flex items-center border border-gray-50 rounded-lg p-2">
+                  <FaEnvelope className="text-[#1e1e2c] mr-2" />
                   <input
                     onChange={inputHandler}
                     value={state.email}
                     name="email"
                     type="email"
                     required
-                    className="bg-white border border-gray-300 w-full text-sm text-gray-800 pl-4 pr-10 py-2.5 rounded-md outline-blue-500"
+                    className="bg-white w-full outline-none"
                     placeholder="Enter email"
                   />
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#bbb"
-                    className="w-4 h-4 absolute right-4"
-                    viewBox="0 0 512 512"
-                  >
-                    <path
-                      d="M339.392 258.624 512 367.872V144.128L339.392 258.624z"
-                      data-original="#000000"
-                    />
-                    <path
-                      d="m0 144.128 172.608 114.496L0 367.872V144.128zM480 80H32c-17.664 0-32 14.336-32 32v288c0 17.664 14.336 32 32 32h448c17.664 0 32-14.336 32-32V112c0-17.664-14.336-32-32-32zm-16.064 32-207.808 137.984L48.064 112h415.872zM32 400V389.856l157.824-102.72 25.6 17.024a56.038 56.038 0 0 0 62.528 0l25.6-17.024L480 389.856V400H32z"
-                      data-original="#000000"
-                    />
-                  </svg>
                 </div>
               </div>
               <div>
-                <label className="text-gray-800 text-sm mb-2 block">
+                <label className="text-[#1e1e2c] text-sm mb-2 block">
                   Password
                 </label>
-                <div className="relative flex items-center">
+                <div className="bg-white flex items-center border border-gray-50 rounded-lg p-2">
+                  <FaLock className="text-[#1e1e2c] mr-2" />
                   <input
                     onChange={inputHandler}
                     value={state.password}
                     name="password"
                     type="password"
                     required
-                    className="bg-white border border-gray-300 w-full text-sm text-gray-800 pl-4 pr-10 py-2.5 rounded-md outline-blue-500"
+                    className="bg-white w-full outline-none"
                     placeholder="Enter password"
                   />
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#bbb"
-                    className="w-4 h-4 absolute right-4"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      d="M15.5 11H15v-1.75C15 7.232 12.988 5 10.5 5S6 7.232 6 9.25V11h-.5a.5.5 0 0 0-.5.5v10c0 .275.225.5.5.5h10a.5.5 0 0 0 .5-.5v-10a.5.5 0 0 0-.5-.5zm-6-1.75C9.5 8.232 9.933 7 10.5 7s1 .232 1 1.25V11h-2V9.25zM13 13c.552 0 1 .447 1 1s-.448 1-1 1-1-.447-1-1 .448-1 1-1zm-3 5c-.552 0-1-.447-1-1s.448-1 1-1 1 .447 1 1-.448 1-1 1zm-1-4c.552 0 1 .447 1 1s-.448 1-1 1-1-.447-1-1 .448-1 1-1zm4 4c-.552 0-1-.447-1-1s.448-1 1-1 1 .447 1 1-.448 1-1 1z"
-                      data-original="#000000"
-                    />
-                  </svg>
                 </div>
               </div>
               <div>
                 <button
                   type="submit"
-                  className="w-full px-4 py-2 text-white font-semibold tracking-wider bg-blue-600 rounded-md hover:bg-blue-700"
+                  className="w-full py-3 px-3 text-sm tracking-wide rounded-lg text-white bg-[#c28f6d] hover:bg-[#a0765d]"
                 >
                   Login
                 </button>
               </div>
               <div>
-                <p className="text-sm text-gray-800">
+                <p className="text-sm text-[#1e1e2c]">
                   Don't have an account?{" "}
                   <Link
                     to="/register"
-                    className="text-blue-600 hover:underline"
+                    className="text-[#c28f6d] hover:underline"
                   >
                     Register
                   </Link>
