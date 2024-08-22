c6d46e282a963702d754ecf86998ef6bbab76110
Abdelkrim7Be <abdelkrimbellagnech99@gmail.com>
2024-08-22 19:19:42 +0100

    Seller Profile #1 : Code Restructured

--- dashboard/src/views/seller/Profile.jsx
+++ dashboard/src/views/seller/Profile.jsx
@@ -7,8 +7,11 @@ import {
   FaRegEdit,
   FaSpinner,
 } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
 
 const Profile = () => {
+  const dispatch = useDispatch();
+  const { userInfo } = useSelector((state) => state.auth);
   const [profileData, setProfileData] = useState({
     shopName: "",
     divisionName: "",
@@ -101,19 +104,19 @@ const Profile = () => {
             <div className="flex flex-col gap-2 p-7 m-2 relative bg-[#f29f6731] rounded-md">
               <div className="flex gap-2">
                 <span className="text-medium font-semibold">Name: </span>
-                <span>Soufiane Boukhari</span>
+                <span>{userInfo.name}</span>
               </div>
               <div className="flex gap-2">
                 <span className="text-medium font-semibold">Email: </span>
-                <span>soufianeboukhari@gmail.com</span>
+                <span>{userInfo.email}</span>
               </div>
               <div className="flex gap-2">
                 <span className="text-medium font-semibold">Role: </span>
-                <span>Seller</span>
+                <span>{userInfo.role}</span>
               </div>
               <div className="flex gap-2">
                 <span className="text-medium font-semibold">Status: </span>
-                <span>Active</span>
+                <span>{userInfo.status}</span>
               </div>
               <div className="flex gap-2">
                 <span className="text-medium font-semibold">
@@ -121,8 +124,8 @@ const Profile = () => {
                 </span>
                 <p>
                   {status === "active" ? (
-                    <span className="text-xs bg-[#55d43531] cursor-pointer font-normal ml-2 px-2 py-0.5 rounded">
-                      Pending
+                    <span className="text-xs bg-[#d4353531] cursor-pointer font-normal ml-2 px-2 py-0.5 rounded">
+                      {userInfo.payment}
                     </span>
                   ) : (
                     <span className="text-xs bg-[#3542d431] cursor-pointer font-normal ml-2 px-2 py-0.5 rounded">
@@ -133,121 +136,126 @@ const Profile = () => {
               </div>
             </div>
           </div>
-          <div className="bg-[#e2e2e2] shadow-md rounded-lg p-4 mb-6">
-            <div className="flex flex-col gap-2 p-7 m-2 relative bg-[#f29f6731] rounded-md">
-              <div className="flex gap-2">
-                <span className="text-medium font-semibold">Shop Name: </span>
-                <span>HMM</span>
+          {userInfo?.shopInfo ? (
+            <>
+              <div className="bg-[#e2e2e2] shadow-md rounded-lg p-4 mb-6 flex-1">
+                <h3 className="text-xl font-semibold mb-4">Update Profile</h3>
+                <form onSubmit={handleProfileSubmit}>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="shopName"
+                      className="block text-[#1e1e2c] mb-1"
+                    >
+                      Shop Name
+                    </label>
+                    <div className="bg-white flex items-center border border-gray-50 rounded-lg p-2">
+                      <FaUser className="text-[#1e1e2c] mr-2" />
+                      <input
+                        id="shopName"
+                        name="shopName"
+                        type="text"
+                        className="w-full outline-none"
+                        placeholder="Shop Name"
+                        onChange={handleProfileChange}
+                        value={profileData.shopName}
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="divisionName"
+                      className="block text-[#1e1e2c] mb-1"
+                    >
+                      Division Name
+                    </label>
+                    <div className="bg-white flex items-center border border-gray-50 rounded-lg p-2">
+                      <FaUser className="text-[#1e1e2c] mr-2" />
+                      <input
+                        id="divisionName"
+                        name="divisionName"
+                        type="text"
+                        className="w-full outline-none"
+                        placeholder="Division Name"
+                        onChange={handleProfileChange}
+                        value={profileData.divisionName}
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="districtName"
+                      className="block text-[#1e1e2c] mb-1"
+                    >
+                      District Name
+                    </label>
+                    <div className="bg-white flex items-center border border-gray-50 rounded-lg p-2">
+                      <FaUser className="text-[#1e1e2c] mr-2" />
+                      <input
+                        id="districtName"
+                        name="districtName"
+                        type="text"
+                        className="w-full outline-none"
+                        placeholder="District Name"
+                        onChange={handleProfileChange}
+                        value={profileData.districtName}
+                      />
+                    </div>
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="subDistrictName"
+                      className="block text-[#1e1e2c] mb-1"
+                    >
+                      Sub-District Name
+                    </label>
+                    <div className="bg-white flex items-center border border-gray-50 rounded-lg p-2">
+                      <FaUser className="text-[#1e1e2c] mr-2" />
+                      <input
+                        id="subDistrictName"
+                        name="subDistrictName"
+                        type="text"
+                        className="w-full outline-none"
+                        placeholder="Sub-District Name"
+                        onChange={handleProfileChange}
+                        value={profileData.subDistrictName}
+                      />
+                    </div>
+                  </div>
+                  <button
+                    type="submit"
+                    className="bg-[#f29f6731] text-[#1e1e2c] font-semibold py-2 px-4 rounded-full"
+                  >
+                    Save Changes
+                  </button>
+                </form>
               </div>
-              <div className="flex gap-2">
-                <span className="text-medium font-semibold">Division: </span>
-                <span>Meknes</span>
-              </div>
-              <div className="flex gap-2">
-                <span className="text-medium font-semibold">District: </span>
-                <span>Hamria</span>
-              </div>
-              <div className="flex gap-2">
-                <span className="text-medium font-semibold">Region: </span>
-                <span>Fez-Meknes</span>
+            </>
+          ) : (
+            <div className="bg-[#e2e2e2] shadow-md rounded-lg p-4 mb-6">
+              <div className="flex flex-col gap-2 p-7 m-2 relative bg-[#f29f6731] rounded-md">
+                <div className="flex gap-2">
+                  <span className="text-medium font-semibold">Shop Name: </span>
+                  <span>HMM</span>
+                </div>
+                <div className="flex gap-2">
+                  <span className="text-medium font-semibold">Division: </span>
+                  <span>Meknes</span>
+                </div>
+                <div className="flex gap-2">
+                  <span className="text-medium font-semibold">District: </span>
+                  <span>Hamria</span>
+                </div>
+                <div className="flex gap-2">
+                  <span className="text-medium font-semibold">Region: </span>
+                  <span>Fez-Meknes</span>
+                </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
 
-        {isEdit && (
-          <div className=" w-full">
-            <div className="bg-[#e2e2e2] shadow-md rounded-lg p-4 mb-6 flex-1">
-              <h3 className="text-xl font-semibold mb-4">Update Profile</h3>
-              <form onSubmit={handleProfileSubmit}>
-                <div className="mb-4">
-                  <label
-                    htmlFor="shopName"
-                    className="block text-[#1e1e2c] mb-1"
-                  >
-                    Shop Name
-                  </label>
-                  <div className="bg-white flex items-center border border-gray-50 rounded-lg p-2">
-                    <FaUser className="text-[#1e1e2c] mr-2" />
-                    <input
-                      id="shopName"
-                      name="shopName"
-                      type="text"
-                      className="w-full outline-none"
-                      placeholder="Shop Name"
-                      onChange={handleProfileChange}
-                      value={profileData.shopName}
-                    />
-                  </div>
-                </div>
-                <div className="mb-4">
-                  <label
-                    htmlFor="divisionName"
-                    className="block text-[#1e1e2c] mb-1"
-                  >
-                    Division Name
-                  </label>
-                  <div className="bg-white flex items-center border border-gray-50 rounded-lg p-2">
-                    <FaUser className="text-[#1e1e2c] mr-2" />
-                    <input
-                      id="divisionName"
-                      name="divisionName"
-                      type="text"
-                      className="w-full outline-none"
-                      placeholder="Division Name"
-                      onChange={handleProfileChange}
-                      value={profileData.divisionName}
-                    />
-                  </div>
-                </div>
-                <div className="mb-4">
-                  <label
-                    htmlFor="districtName"
-                    className="block text-[#1e1e2c] mb-1"
-                  >
-                    District Name
-                  </label>
-                  <div className="bg-white flex items-center border border-gray-50 rounded-lg p-2">
-                    <FaUser className="text-[#1e1e2c] mr-2" />
-                    <input
-                      id="districtName"
-                      name="districtName"
-                      type="text"
-                      className="w-full outline-none"
-                      placeholder="District Name"
-                      onChange={handleProfileChange}
-                      value={profileData.districtName}
-                    />
-                  </div>
-                </div>
-                <div className="mb-4">
-                  <label
-                    htmlFor="subDistrictName"
-                    className="block text-[#1e1e2c] mb-1"
-                  >
-                    Sub-District Name
-                  </label>
-                  <div className="bg-white flex items-center border border-gray-50 rounded-lg p-2">
-                    <FaUser className="text-[#1e1e2c] mr-2" />
-                    <input
-                      id="subDistrictName"
-                      name="subDistrictName"
-                      type="text"
-                      className="w-full outline-none"
-                      placeholder="Sub-District Name"
-                      onChange={handleProfileChange}
-                      value={profileData.subDistrictName}
-                    />
-                  </div>
-                </div>
-                <button
-                  type="submit"
-                  className="bg-[#f29f6731] text-[#1e1e2c] font-semibold py-2 px-4 rounded-full"
-                >
-                  Save Changes
-                </button>
-              </form>
-            </div>
+        <div className=" w-full">
+          {isEdit && (
             <div className="bg-[#e2e2e2] shadow-md rounded-lg p-4 mb-6">
               <h3 className="text-lg font-semibold mb-4">Change Password</h3>
               <form onSubmit={handlePasswordSubmit}>
@@ -319,8 +327,8 @@ const Profile = () => {
                 </button>
               </form>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
