ce6b98b5233eb87cdc47e30d2c140d4d4ff70301
Abdelkrim7Be <abdelkrimbellagnech99@gmail.com>
2024-08-27 00:10:06 +0100

    Admin Login Template : Some Enhancements

--- dashboard/src/views/auth/AdminLogin.jsx
+++ dashboard/src/views/auth/AdminLogin.jsx
@@ -46,7 +46,7 @@ const AdminLogin = () => {
     <div className="bg-gray-50 font-[sans-serif]">
       <div className="min-h-screen flex flex-col items-center justify-center py-6 px-4">
         <div className="max-w-md w-full">
-          <div className="p-8 rounded-2xl bg-white shadow">
+          <div className="p-8 rounded-2xl bg-[#f0efef] shadow">
             <div>
               <img
                 src="/images/fullLogo.png"
@@ -64,20 +64,11 @@ const AdminLogin = () => {
                     name="email"
                     type="email"
                     required
-                    className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
+                    className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md bg-white focus:outline-[#814500]"
                     placeholder="Enter email"
                     onChange={inputHandler}
                     value={state.email}
                   />
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#bbb"
-                    className="w-4 h-4 absolute right-4"
-                    viewBox="0 0 512 512"
-                  >
-                    <path d="M339.392 258.624 512 367.872V144.128L339.392 258.624z" />
-                    <path d="M0 144.128L172.608 258.624 0 367.872V144.128zM480 80H32c-17.664 0-32 14.336-32 32v288c0 17.664 14.336 32 32 32h448c17.664 0 32-14.336 32-32V112c0-17.664-14.336-32-32-32zm-16.064 32-207.808 137.984L48.064 112h415.872zM32 400V389.856l157.824-102.72 25.6 17.024a56.038 56.038 0 0 0 62.528 0l25.6-17.024L480 389.856V400H32z" />
-                  </svg>
                 </div>
               </div>
 
@@ -90,19 +81,11 @@ const AdminLogin = () => {
                     name="password"
                     type="password"
                     required
-                    className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md outline-blue-600"
+                    className="w-full text-gray-800 text-sm border border-gray-300 px-4 py-3 rounded-md bg-white focus:outline-[#814500]"
                     placeholder="Enter password"
                     onChange={inputHandler}
                     value={state.password}
                   />
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="#bbb"
-                    className="w-4 h-4 absolute right-4 cursor-pointer"
-                    viewBox="0 0 128 128"
-                  >
-                    <path d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z" />
-                  </svg>
                 </div>
               </div>
 
@@ -112,7 +95,7 @@ const AdminLogin = () => {
                     id="remember-me"
                     name="remember-me"
                     type="checkbox"
-                    className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+                    className="h-4 w-4 shrink-0 text-[#c28f6d]  border-gray-300 rounded"
                   />
                   <label
                     htmlFor="remember-me"
@@ -122,7 +105,7 @@ const AdminLogin = () => {
                   </label>
                 </div>
                 <div className="text-sm">
-                  <a href="#" className="text-blue-600 hover:underline">
+                  <a href="#" className="text-[#c28f6d] hover:underline">
                     Forgot your password?
                   </a>
                 </div>
@@ -132,7 +115,7 @@ const AdminLogin = () => {
                 <button
                   disabled={loader ? true : false}
                   type="submit"
-                  className="w-full py-3 px-3 text-sm tracking-wide rounded-lg text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
+                  className="w-full py-3 px-3 text-sm tracking-wide rounded-lg text-white bg-[#c28f6d] hover:bg-[#a0765d] focus:outline-none"
                 >
                   {loader ? <LoadingSpinner /> : "Sign in"}
                 </button>
@@ -141,7 +124,7 @@ const AdminLogin = () => {
                 Don't have an account?{" "}
                 <Link
                   to="/register"
-                  className="text-blue-600 hover:underline ml-1 whitespace-nowrap"
+                  className="text-[#c28f6d] hover:underline ml-1 whitespace-nowrap"
                 >
                   Register here
                 </Link>
